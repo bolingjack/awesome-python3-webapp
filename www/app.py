03eee88f6c7b57106becfd6250639a020fbc644f
yuyingqi <yuyingqi@goldwind.com.cn>
2018-01-25 18:01:16 +0800

    study orm

--- www/app.py
+++ www/app.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from aiohttp import web
 
 
-def index(request):
+async def index(request):
     return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
 
 
