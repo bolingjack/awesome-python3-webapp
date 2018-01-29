c88db3b8d139feb5cef8694460ee2575ff79a8d8
yuyingqi <yuyingqi@goldwind.com.cn>
2018-01-29 18:27:13 +0800

    config

---
+++ www/config_default.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+'config_default'
+
+__author__ = 'yuyingqi'
+__email__ = 'yuyingqijack@163.com'
+__version__ = 1.0
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www',
+        'password': 'www',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'awesome'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+'config_default'
+
+__author__ = 'yuyingqi'
+__email__ = 'yuyingqijack@163.com'
+__version__ = 1.0
+
+configs = {
+    'db': {
+        'host': '192.168.0.100'
+    }
+}
