1a21d3ac1d7d644211caf75014adb27ae6f6c4ec
yuyingqi <yuyingqi@goldwind.com.cn>
2018-01-31 18:38:16 +0800

    create blog

--- www/handlers.py
+++ www/handlers.py
@@ -13,7 +13,7 @@ import markdown2
 from aiohttp import web
 
 from coreweb import get, post
-from apis import APIValueError, APIResourceNotFoundError, APIError
+from apis import APIValueError, APIResourceNotFoundError, APIError, APIPermissionError
 
 from models import User, Comment, Blog, next_id
 from config import configs
@@ -22,14 +22,36 @@ COOKIE_NAME = 'awesession'
 _COOKIE_KEY = configs.session.secret
 
 
-def user2cookie(user, max_age):#通过用户信息获得cookie
+def check_admin(request):
+    if request.__user__ is None or not request.__user__.admin:
+        raise APIPermissionError()
+
+
+def get_page_index(page_str):
+    p = 1
+    try:
+        p = int(page_str)
+    except ValueError as e:
+        pass
+    if p < 1:
+        p = 1
+    return p
+
+
+def user2cookie(user, max_age):  # 通过用户信息获得cookie
     expires = str(int(time.time() + max_age))  # 过期时间
     s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
     L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
     return '-'.join(L)
 
 
-async def cookie2user(cookie_str):#通过cookie获取用户信息
+def text2html(text):
+    lines = map(lambda s: '<p>%s</p>' % s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;'),
+                filter(lambda s: s.strip() != '', text.split('\n')))
+    return ''.join(lines)
+
+
+async def cookie2user(cookie_str):  # 通过cookie获取用户信息
     if not cookie_str:
         return None
     try:
@@ -67,6 +89,15 @@ async def index(request):
     }
 
 
+@get('/blog/{id}')
+async def get_blog(id):
+    blog = await Blog.find(id)
+    comments = await Comment.findAll('blog_id =?', [id], orderBy='created_at desc')
+    for c in comments:
+        c.html_content = text2html(c.content)
+        blog.html_content = markdown2.markdown(blog.content)
+
+
 @get('/register')
 async def register():
     return {
@@ -82,7 +113,7 @@ async def signin():
 
 
 @post('/api/authenticate')
-async def authenticate(*, email, passwd):#登录
+async def authenticate(*, email, passwd):  # 登录
     if not email:
         raise APIValueError('email', 'Invalid email.')
     if not passwd:
@@ -114,6 +145,15 @@ async def signout(request):
     return r
 
 
+@get('/manage/blogs/create')
+def manage_create_blog():
+    return {
+        '__template__': 'manage_blog_edit.html',
+        'id': '',
+        'action': 'api/blogs'
+    }
+
+
 _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
 _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
 
@@ -133,7 +173,7 @@ async def api_register_user(*, email, name, passwd):
         raise APIError('register:failed', 'email', 'email is already in use.')
     uid = next_id()
     sha1_passwd = '%s:%s' % (uid, passwd)
-    user = User(id=uid, name=name.strip(), email = email,passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(),
+    user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(),
                 image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
     await user.save()
     r = web.Response()
@@ -142,3 +182,24 @@ async def api_register_user(*, email, name, passwd):
     r.content_type = 'application/json'
     r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
     return r
+
+
+@get('/api/blogs/{id}')
+async def api_get_blog(*, id):
+    blog = await Blog.find(id)
+    return blog
+
+
+@post('/api/blogs')
+async def api_create_blog(request, *, name, summary, content):
+    check_admin(request)
+    if not name or not name.strip():
+        raise APIValueError('name', 'name cannot be empty.')
+    if not summary or not summary.strip():
+        raise APIValueError('summary', 'summary cannot be empty.')
+    if not content or not content.strip():
+        raise APIValueError('content', 'content cannot be empty.')
+    blog = Blog(user_id=request.__user__.id, user_name=request.__user__.name, user_image=request.__user__.image,
+                name=name.strip(), summary=summary.strip(), content=content.strip())
+    await blog.save()
+    return blog
